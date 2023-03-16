71e6b175ad84a8d7f8992eefad354406b4ea1e41
Alekiie <amnjogu78@gmail.com>
2023-03-16 21:14:40 +0300

    Menu corrections

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ const menu = document.querySelector("#menu");
 const body = document.querySelector("body");
 const moon = document.querySelector("#moon");
 const hamburger = document.querySelector("#hamburger");
-const hLinks = document.querySelectorAll('#hLink');
+const hLinks = document.querySelectorAll("#hLink");
 
 hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  if (menu.classList.contains("hidden")) {
+    hamburger.innerHTML = `<img src="./images/menu.png" id="hamburger" class="md:hidden relative w-5 h-5 font-bold z-20 cursor-pointer " alt="">`;
+  } else {
+    hamburger.innerHTML = `<i class="fa fa-times text-2xl text-white cursor-pointer "></i>`;
+  }
+});
+hLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     menu.classList.toggle("hidden");
     if (menu.classList.contains("hidden")) {
-        hamburger.innerHTML = `<img src="./images/menu.png" id="hamburger" class="md:hidden w-5 h-5 font-bold z-20 cursor-pointer" alt="">`;
+      hamburger.innerHTML = `<img src="./images/menu.png" id="hamburger" class="md:hidden w-5 h-5 font-bold z-20 cursor-pointer" alt="">`;
     } else {
-        hamburger.innerHTML = `<i class="fa fa-times text-2xl text-white cursor-pointer "></i>`;
+      hamburger.innerHTML = `<i class="fa fa-times text-2xl text-white cursor-pointer "></i>`;
     }
-})
-hLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        menu.classList.toggle("hidden");
-        if (menu.classList.contains("hidden")) {
-            hamburger.innerHTML = `<img src="./images/menu.png" id="hamburger" class="md:hidden w-5 h-5 font-bold z-20 cursor-pointer" alt="">`;
-        } else {
-            hamburger.innerHTML = `<i class="fa fa-times text-2xl text-white cursor-pointer "></i>`;
-        }
-    })
-})
+  });
+});
 moon.addEventListener("click", () => {
-    body.classList.toggle("dark")
-})
+  body.classList.toggle("dark");
+});
