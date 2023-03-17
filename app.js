72587da3c0018533b6e008c90b593c8033f3f6d2
Alekiie <amnjogu78@gmail.com>
2023-03-17 10:44:29 +0300

    Some bugs

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ hLinks.forEach((link) => {
     if (menu.classList.contains("hidden")) {
       hamburger.innerHTML = `<img src="./images/menu.png" id="hamburger" class="md:hidden w-5 h-5 font-bold z-20 cursor-pointer" alt="">`;
     } else {
-      hamburger.innerHTML = `<i class="fa fa-times text-2xl text-white cursor-pointer "></i>`;
+      hamburger.innerHTML = `<i class="fa fa-times text-2xl text-white cursor-pointer absolute flex "></i>`;
     }
   });
 });
